f73d3d2607d741e05c4ebe089158b77823101e18
Martin Fink <martin@finkmartin.com>
2023-08-04 14:22:29 +0000

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,14 +1,5 @@
 # source https://nixos.wiki/wiki/LLVM
-{ pkgs ? import (fetchTarball "https://github.com/PBHDK/nixpkgs/archive/2eac0c6990719e80fd56d89b1f48e7a5bf59b843.tar.gz") { }
-}:
-let
-  myPython = pkgs.python310.withPackages (ps: with ps; [
-    autopep8
-    html5lib
-    pyyaml
-    pygments
-  ]);
-in
+with import <nixpkgs> { };
 pkgs.llvmPackages_latest.stdenv.mkDerivation {
   name = "llvm-debug-env";
   nativeBuildInputs = [
@@ -18,7 +9,6 @@ pkgs.llvmPackages_latest.stdenv.mkDerivation {
     pkgs.cmake
     pkgs.ninja
     pkgs.graphviz
-    myPython
     pkgs.llvmPackages_latest.lld
     pkgs.llvmPackages_latest.lldb
     pkgs.pkg-config
@@ -26,9 +16,6 @@ pkgs.llvmPackages_latest.stdenv.mkDerivation {
   ];
   buildInputs = [ pkgs.zlib ];
   hardeningDisable = [ "all" ];
-  shellHook = ''
-    export PYTHONPATH='lldb -P'
-  '';
   PATH_TO_CLANG = "${pkgs.llvmPackages_latest.stdenv.cc}/bin/clang++";
   # FIXME why is this not included?
   NIX_LDFLAGS = "-rpath ${pkgs.zlib}/lib";
