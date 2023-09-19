19455b88652ebaf83a392ba0f66af4aa751bed04
fritzrehde <fritzrehde.work@gmail.com>
2023-09-19 22:52:20 +0000

    Extended demo program

--- demo/deterministic-stack-tagging/main.c
+++ demo/deterministic-stack-tagging/main.c
@@ -6,24 +6,32 @@ int main(int argc, char **argv) {
 		printf("Usage: %s integer\n", argv[0]);
 		return 1;
 	}
+    printf("argc = %d\n", argc);
 	int integer = atoi(argv[1]);
 
     printf("integer = %d\n", integer);
 
-    printf("expect random tag (first stack) X\n");
     int stack_alloc_1[10];
-    int x = integer + 42;
-    printf("expect incremented tag X += 1\n");
-    int stack_alloc_2[20];
-    printf("expect random tag (heap) Y\n");
-    int *heap_alloc_1 = (int*)malloc(sizeof(int) * 4);
-    printf("expect incremented tag X += 1\n");
-    int stack_alloc_3[20];
+    printf("expect random tag (first stack) X; stack_alloc_1 = %p\n", stack_alloc_1);
 
+    int x = integer + 42;
+
+    int stack_alloc_2[20];
+    printf("expect incremented tag X += 1; stack_alloc_2 = %p\n", stack_alloc_2);
+
+    // int *heap_alloc_1 = (int*)malloc(sizeof(int) * 4);
+    // printf("expect random tag (heap) Y; heap_alloc2 = %p\n", heap_alloc_1);
+
+    int stack_alloc_3[20];
+    printf("expect incremented tag X += 1; stack_alloc_3 = %p\n", stack_alloc_3);
+
+    // Do something with the arrays, to prevent being optimized away
     stack_alloc_1[0] = integer;
     stack_alloc_2[0] = integer;
     stack_alloc_3[0] = integer;
-    heap_alloc_1[0] = integer;
+    // heap_alloc_1[0] = integer;
+
+    // free(heap_alloc_1);
 
     return 0;
 }
